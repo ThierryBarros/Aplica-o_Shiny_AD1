a656b2a909c17454cbd848d520e0eaea06eb7aa3
Thierry Barros <thierry.barros@ccc.ufcg.edu.br>
2018-06-13 03:49:35 -0300

    shiny last version

--- vias-app/server.R
+++ vias-app/server.R
@@ -6,10 +6,12 @@
 # 
 #    http://shiny.rstudio.com/
 #
-
 library(shiny)
 library(tidyverse)
 library(here)
+library(ggplot2)
+library(rsconnect)
+library(plotly)
 source(here("code/read_wrangle.R"))
 
 vias = read_wrangle_data()
@@ -23,21 +25,33 @@ profissoes_nos_dados = vias %>%
 shinyServer(function(input, output) {
     prof_selecionada = reactive({input$profissao})
     
+
     output$distPlot <- renderPlot({
-        vias_profissao = vias %>% filter(profissao == prof_selecionada())
+        vias_profissao = vias %>% filter(comprimento<input$tamanho & arvore<input$arvore & profissao == prof_selecionada())
         vias_profissao %>% 
-            ggplot(aes(x = arvores_100m_mean)) + 
+            ggplot(aes(x = pontoonibu)) + 
             geom_histogram(binwidth = 1, 
                            boundary = 0, 
                            fill = "darkgreen") + 
-            scale_x_continuous(limits = c(0, input$bins))
+            scale_x_continuous(limits = c(0, input$bins)) + ggtitle(paste("Histograma dos pontos de ônibus de Campina Grande para profissão:",toString(prof_selecionada())) )  + xlab("quantidade de pontos de ônibus") + ylab("numero de ruas")
     })
+    output$dPlot <- renderPlot({
+        vias_profissao = vias %>% filter(comprimento<input$tamanho & arvore<input$arvore & profissao == prof_selecionada())
+        plot_ly(vias_profissao,x = vias_profissao$comprimento, y =vias_profissao$arvore, 
+                text = paste("Onibus: ",vias_profissao$pontoonibu),
+                mode = "markers", color = vias_profissao$arvores_100m_mean, size = vias_profissao$pontoonibu) 
+        
+        
+    })
+ 
     
     output$listagem <- renderTable({
         vias %>% 
-            filter(profissao == prof_selecionada()) %>% 
+            filter(comprimento<input$tamanho & arvore<input$arvore & profissao == prof_selecionada()) %>% 
             select(nome = nomelograd, 
-                   comprimento)
+                   comprimento, arvore) %>% arrange(desc(comprimento),desc(arvore)) %>% head(input$numero) 
+           
+        
     })
     
 })
--- vias-app/ui.R
+++ vias-app/ui.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(tidyverse)
 library(here)
+library(rsconnect)
 source(here("code/read_wrangle.R"))
 
 profissoes_nos_dados = read_wrangle_data() %>% 
@@ -8,13 +9,14 @@ profissoes_nos_dados = read_wrangle_data() %>%
     pull(profissao) %>% 
     unique()
 
-
+comprimento = read_wrangle_data() %>% select(comprimento)
+arvore = read_wrangle_data() %>% select(arvore)
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Ruas mais arborizadas por profissão"),
-  
+  titlePanel("Ruas de Campina Grande"),
+  h3("Veja como é a relação da ruas com o nome da profissão, o número de árvores em cada rua, seu comprimento e número de pontos de ônibus. Dependendo da profissão as características tem ou não forte relação. No final tem uma lista das ruas daquela profissão em ordem crescente de comprimento e número de árvores."),
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
@@ -22,20 +24,32 @@ shinyUI(fluidPage(
                     "Profissão", 
                     choices = profissoes_nos_dados), 
         sliderInput("bins",
-                    "Number of bins:",
+                    "Number de pontos de onibus:",
                     min = 1,
-                    max = 50,
-                    value = 30)
+                    max = 100,
+                    value = 30),
+        sliderInput("tamanho",
+                    "Comprimento máximo das ruas",
+                    min = as.numeric(comprimento[which.min(comprimento$comprimento),1]),
+                    max = as.numeric(comprimento[which.max(comprimento$comprimento),1]),
+                    value = 1000),
+        sliderInput("arvore",
+                    "Número máximo de arvores:",
+                    min = as.numeric(arvore[which.min(arvore$arvore),1]),
+                    max = as.numeric(arvore[which.max(arvore$arvore),1]),
+                    value = 20),
+        numericInput("numero","Número de ruas:",5,1,50)
         
     ),
     
     # Show a plot of the generated distribution
     mainPanel(
        plotOutput("distPlot"), 
+       plotOutput("dPlot"), 
        tableOutput("listagem")
     )
-  ),
-  radioButtons("escolha",
-               "Escolhas",
-               c("Escolha a","Escolha b"))
+  )
+
+  
+  
 ))
